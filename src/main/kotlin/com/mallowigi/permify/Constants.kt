e6524b11440ca6bb9a6e2609cee4bcb1b8472847
Elior <heliosaian@gmail.com>
2024-06-13 08:13:29 +0300

    remove internal

--- src/main/kotlin/com/mallowigi/permify/Constants.kt
+++ src/main/kotlin/com/mallowigi/permify/Constants.kt
@@ -1,7 +1,10 @@
 package com.mallowigi.permify
 
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiFile
 
 const val PERMIFY: String = "Permify"
 
 fun isPermifyFile(file: VirtualFile): Boolean = file.extension in setOf("perm", "permify")
+
+fun isPermifyFile(file: PsiFile): Boolean = file.name.endsWith(".perm") || file.name.endsWith(".permify")
--- src/main/kotlin/com/mallowigi/permify/PermifyTemplateContextType.kt
+++ src/main/kotlin/com/mallowigi/permify/PermifyTemplateContextType.kt
@@ -3,11 +3,9 @@ package com.mallowigi.permify
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-@Suppress("UnstableApiUsage")
 class PermifyTemplateContextType : TemplateContextType(PERMIFY) {
   override fun isInContext(context: TemplateActionContext): Boolean {
-    val file = context.editor?.virtualFile ?: return false
 
-    return isPermifyFile(file)
+    return isPermifyFile(context.file)
   }
 }
